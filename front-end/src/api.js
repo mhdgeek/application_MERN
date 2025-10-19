0341e06494e3db96a492fb9848d05d3988bbba02
Mohamed <105009551+mhdgeek@users.noreply.github.com>
2025-10-19 16:04:03 +0000

    Add API functions for fetching and saving smartphones

---
+++ front-end/src/api.js
@@ -0,0 +1,24 @@
+// front-end/src/api.js
+import axios from "axios";
+
+const API_URL = "http://localhost:30000/api/smartphones"; // backend exposé via NodePort
+
+export const getSmartphones = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error("❌ API Response Error:", error);
+    throw error;
+  }
+};
+
+export const saveSmartphone = async (smartphone) => {
+  try {
+    const response = await axios.post(API_URL, smartphone);
+    return response.data;
+  } catch (error) {
+    console.error("Error saving smartphone: ", error);
+    throw error;
+  }
+};
